fb7da7d4c559536631dd8c70c6243940f7a50a13
“Kojo <kodeon6@gmail.com>
2016-07-07 02:34:16 +0100

    Styling both show page and devise views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @legal = Legal.find(params[:legal_id])
+    @comment = @legal.comments.create(comment_params)
+    redirect_to legal_path(@legal)
+  end
+
+  def destroy
+    @legal = Legal.find(params[:legal_id])
+    @comment = @legal.comments.find(params[:id])
+    @comment.destroy
+    redirect_to legal_path(@legal)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :legals do
-   resources :comments
- end
+    resources :comments
+  end
  root 'legals#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
